cfcea76eb4ba0b6c3a58768725710013c57d2024
Ali <ali.abolfathee@yahoo.com>
2024-08-14 02:02:15 +0330

    update

---
+++ test/police_department_test.py
@@ -0,0 +1,8 @@
+from model.entity.employee_information import EmployeeInformation
+from datetime import datetime
+
+date1 = datetime.now()
+date2 = datetime.now()
+
+employee = EmployeeInformation(1,"ali","abolfathi",date1,date2,"tehran")
+print(employee)
